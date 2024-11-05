86cb088a9c9547041031524651b68541ba003102
Vikram Kumar Choudhary <vikram0104irctc@gmail.com>
2024-11-05 11:02:12 +0530

    changes the name of enviroment variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  define: {
-    "process.env": process.env,
-  },
   plugins: [react()],
   resolve: {
     alias: {
