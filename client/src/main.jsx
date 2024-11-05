a566a7071adea7c95d8f50a03d9e5798f69edf7b
Vikram Kumar Choudhary <vikram0104irctc@gmail.com>
2024-11-05 10:47:33 +0530

    changes the name of enviroment variable

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
